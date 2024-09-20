329001f5c6424b18f0b2cdb6fc332a323c8f399b
Akter Uzzaman <akteruzzaman216@gmail.com>
2024-09-20 10:49:42 +0600

    bottom nav and custom marker added

--- app/src/main/java/com/garageKoi/garage/MainActivity.kt
+++ app/src/main/java/com/garageKoi/garage/MainActivity.kt
@@ -62,9 +62,7 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
             isTiltGesturesEnabled = false
         }
         checkLocationPermission()
-        addCustomMarker(24.741450314569484, 89.82932144162278)
-        addCustomMarker(24.739487050434985, 89.8324128322312)
-        addCustomMarker(24.743060740972016, 89.8244501069644)
+        setupMarkers()
     }
 
     private fun checkLocationPermission() {
@@ -125,6 +123,78 @@ class MainActivity : BaseActivity(), OnMapReadyCallback {
         }
 
         setCustomInfoWindow()
+    }
+
+
+    private fun setupMarkers() {
+        // Dhaka city locations with their approximate latitude and longitude
+        val dhakaLocations = listOf(
+            Pair("Adabor", LatLng(23.7663, 90.3580)),
+            Pair("Uttara", LatLng(23.8749, 90.4007)),
+            Pair("Mirpur", LatLng(23.8040, 90.3667)),
+            Pair("Pallabi", LatLng(23.8223, 90.3654)),
+            Pair("Kazipara", LatLng(23.8112, 90.3675)),
+            Pair("Kafrul", LatLng(23.7881, 90.3740)),
+            Pair("Agargaon", LatLng(23.7743, 90.3797)),
+            Pair("Sher-e-Bangla Nagar", LatLng(23.7623, 90.3798)),
+            Pair("Cantonment Area", LatLng(23.8274, 90.4109)),
+            Pair("Banani", LatLng(23.7880, 90.4001)),
+            Pair("Gulshan", LatLng(23.7925, 90.4078)),
+            Pair("Niketan", LatLng(23.7845, 90.4112)),
+            Pair("Shahjadpur", LatLng(23.7964, 90.4170)),
+            Pair("Mohakhali", LatLng(23.7784, 90.4055)),
+            Pair("Bashundhara", LatLng(23.8173, 90.4272)),
+            Pair("Banasree", LatLng(23.7637, 90.4316)),
+            Pair("Aftab Nagar", LatLng(23.7640, 90.4360)),
+            Pair("Baridhara", LatLng(23.8103, 90.4335)),
+            Pair("Uttarkhan", LatLng(23.8907, 90.4310)),
+            Pair("Dakshinkhan", LatLng(23.8697, 90.4269)),
+            Pair("Bawnia", LatLng(23.8798, 90.4095)),
+            Pair("Khilkhet", LatLng(23.8287, 90.4259)),
+            Pair("Tejgaon", LatLng(23.7630, 90.4000)),
+            Pair("Farmgate", LatLng(23.7557, 90.3824)),
+            Pair("Mohammadpur", LatLng(23.7623, 90.3587)),
+            Pair("Rampura", LatLng(23.7580, 90.4210)),
+            Pair("Badda", LatLng(23.7802, 90.4274)),
+            Pair("Satarkul", LatLng(23.8289, 90.4364)),
+            Pair("Beraid", LatLng(23.8353, 90.4447)),
+            Pair("Khilgaon", LatLng(23.7526, 90.4283)),
+            Pair("Vatara", LatLng(23.8184, 90.4491)),
+            Pair("Gabtali", LatLng(23.7904, 90.3294))
+        )
+
+        val customHue = 154f
+        // Add markers for each location in Dhaka
+        for (location in dhakaLocations) {
+            val markerOptions = MarkerOptions()
+                .position(location.second)
+                .icon(BitmapDescriptorFactory.defaultMarker(customHue))
+                .title(location.first)
+
+            googleMap.addMarker(markerOptions)
+        }
+
+        // Coordinates for the 7 divisions of Bangladesh
+        val divisionLocations = listOf(
+            Pair("Dhaka Division", LatLng(23.8103, 90.4125)),
+            Pair("Chittagong Division", LatLng(22.3569, 91.7832)),
+            Pair("Rajshahi Division", LatLng(24.3636, 88.6241)),
+            Pair("Sylhet Division", LatLng(24.9045, 91.8611)),
+            Pair("Khulna Division", LatLng(22.8456, 89.5403)),
+            Pair("Barisal Division", LatLng(21.4272, 92.0058)),
+            Pair("Rangpur Division", LatLng(25.7439, 89.2752)),
+            Pair("Mymensingh Division", LatLng(24.7499, 90.4071))
+        )
+
+        // Add markers for each division
+        for (division in divisionLocations) {
+            val markerOptions = MarkerOptions()
+                .position(division.second)
+                .icon(BitmapDescriptorFactory.defaultMarker(customHue))
+                .title(division.first)
+
+            googleMap.addMarker(markerOptions)
+        }
     }
 
 
